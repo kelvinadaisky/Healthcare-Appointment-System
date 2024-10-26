3976f55d333ebe89ce17d05ec1a71e5f8f751ba0
kelvinadaisky <kelvinadaiskyirutingabo2002@gmail.com>
2024-10-26 08:05:24 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_prog_Project.Models;
+
+namespace Web_prog_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Assistants()
+        {
+            return View();
+        }
+
+        public IActionResult Professors()
+        {
+            return View();
+        }
+
+        public IActionResult Departments()
+        {
+            return View();
+        }
+
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+
+        public IActionResult Appointments()
+        {
+            return View();
+        }
+
+        public IActionResult Emergencies()
+        {
+            return View();
+        }
+
+    }
+}
